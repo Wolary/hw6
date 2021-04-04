21b2d9c396e8ef48e14c84bfff0a4d5cb67f820a
Wolary <Ktrnjh19>
2021-04-04 16:22:30 +0300

    Added

--- src/test/java/utils/ZipFiles.java
+++ src/test/java/utils/ZipFiles.java
@@ -5,8 +5,7 @@ import net.lingala.zip4j.exception.ZipException;
 
 public class ZipFiles {
 
-
-    public static void unZip (String path, String unzippath,String pas) throws ZipException {
+    public static void unZip (String path, String unzippath, String pas) throws ZipException {
         ZipFile zipFile = new ZipFile((path));
         if (zipFile.isEncrypted()) {
             zipFile.setPassword(pas);
